731d96ae480a5a37c360aa53e05d8c00292287e4
HuyPham <phamminhhuy0901tk@gmail.com>
2025-12-26 18:09:54 +0700

    Create all table

---
+++ Nhom12_TIMKIEMTHONGTINBACSI.sql
@@ -0,0 +1,190 @@
+﻿--Kiem tra xem database đa ton ti hay chưa, ton tai th? xóa
+USE master;
+IF EXISTS (SELECT name FROM sys.databases WHERE name = 'TimKiemBacSi')
+BEGIN
+    ALTER DATABASE TimKiemBacSi SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE TimKiemBacSi;
+END;
+--tao database TimKiemBacSi
+CREATE DATABASE TimKiemBacSi
+GO
+USE TimKiemBacSi
+-- TỈNH THÀNH
+CREATE TABLE TinhThanh (
+    IdTinhThanh INT IDENTITY PRIMARY KEY,
+    TenTinhThanh NVARCHAR(100) NOT NULL
+);
+-- PHƯỜNG XÃ
+CREATE TABLE PhuongXa (
+    IdPhuongXa INT IDENTITY PRIMARY KEY,
+    TenPhuongXa NVARCHAR(100),
+    IdTinhThanh INT,
+    FOREIGN KEY (IdTinhThanh) REFERENCES TinhThanh(IdTinhThanh)
+);
+-- BỆNH VIỆN KHU PHÒNG
+CREATE TABLE BenhVien (
+    IdBenhVien INT IDENTITY PRIMARY KEY,
+    TenBenhVien NVARCHAR(200),
+    HotLine VARCHAR(20),
+    Email VARCHAR(100),
+    MoTa NVARCHAR(MAX),
+    DiaDiem NVARCHAR(255),
+    IdPhuongXa INT,
+    FOREIGN KEY (IdPhuongXa) REFERENCES PhuongXa(IdPhuongXa)
+);
+CREATE TABLE Khu (
+    IdKhu INT IDENTITY PRIMARY KEY,
+    TenKhu NVARCHAR(100),
+    IdBenhVien INT,
+    FOREIGN KEY (IdBenhVien) REFERENCES BenhVien(IdBenhVien)
+);
+CREATE TABLE Phong (
+    IdPhong INT IDENTITY PRIMARY KEY,
+    TenPhong NVARCHAR(100),
+    Tang INT,
+    IdKhu INT,
+    FOREIGN KEY (IdKhu) REFERENCES Khu(IdKhu)
+);
+----------------------------------------------------------------NGƯỜI DÙNG----------------------------------------------
+CREATE TABLE BacSi (
+    IdBacSi INT IDENTITY PRIMARY KEY,
+    SoDienThoai VARCHAR(15),
+    HoTen NVARCHAR(100),
+    Email VARCHAR(100),
+    MatKhau VARCHAR(255),
+    NgaySinh DATE,
+    GioiTinh NVARCHAR(10),
+    BangCap NVARCHAR(100),
+    NamKinhNghiem INT,
+    ChungChiHanhNghe NVARCHAR(255),
+    ThanhTuu NVARCHAR(MAX),
+    MoTa NVARCHAR(MAX),
+    AnhDaiDien NVARCHAR(255),
+    NoiLamViec NVARCHAR(255),
+    CCCD VARCHAR(20)
+);
+CREATE TABLE BenhNhan (
+    IdBenhNhan INT IDENTITY PRIMARY KEY,
+    SoDienThoai VARCHAR(15),
+    HoTen NVARCHAR(100),
+    Email VARCHAR(100),
+    MatKhau VARCHAR(255),
+    NgaySinh DATE,
+    GioiTinh NVARCHAR(10),
+    NgayDangKy DATE,
+    DiaDiem NVARCHAR(255),
+    CCCD VARCHAR(20) NULL
+);
+CREATE TABLE CanBoHanhChinh (
+    IdCanBo INT IDENTITY PRIMARY KEY,
+    SoDienThoai VARCHAR(15),
+    HoTen NVARCHAR(100),
+    Email VARCHAR(100),
+    MatKhau VARCHAR(255),
+    NgaySinh DATE,
+    GioiTinh NVARCHAR(10),
+    ChucVu NVARCHAR(100),
+    NoiLamViec NVARCHAR(255),
+    CCCD VARCHAR(20)
+);
+------------------------------------------------------CHUYÊN KHOA-------------------------------------
+CREATE TABLE ChuyenKhoa (
+    IdChuyenKhoa INT IDENTITY PRIMARY KEY,
+    TenChuyenKhoa NVARCHAR(150),
+    MoTa NVARCHAR(MAX)
+);
+CREATE TABLE ChuyenKhoa_BacSi (
+    IdBacSi INT,
+    IdChuyenKhoa INT,
+    PRIMARY KEY (IdBacSi, IdChuyenKhoa),
+    FOREIGN KEY (IdBacSi) REFERENCES BacSi(IdBacSi),
+    FOREIGN KEY (IdChuyenKhoa) REFERENCES ChuyenKhoa(IdChuyenKhoa)
+);
+------------------------------------------------------LỊCH LÀM VIỆC-------------------------------------
+CREATE TABLE LichLamViec (
+    IdLichLamViec INT IDENTITY PRIMARY KEY,
+    NgayLamViec DATE,
+    KhungGio NVARCHAR(50),
+    TrangThai NVARCHAR(50),
+    IdBacSi INT,
+    IdPhong INT,
+    FOREIGN KEY (IdBacSi) REFERENCES BacSi(IdBacSi),
+    FOREIGN KEY (IdPhong) REFERENCES Phong(IdPhong)
+);
+------------------------------------------------------THÔNG BÁO-------------------------------------
+CREATE TABLE ThongBao (
+    IdThongBao INT IDENTITY PRIMARY KEY,
+    TieuDe NVARCHAR(255),
+    NoiDung NVARCHAR(MAX),
+    NgayGui DATETIME,
+    LoaiThongBao NVARCHAR(100)
+);
+
+CREATE TABLE ThongBao_BacSi (
+    IdBacSi INT,
+    IdThongBao INT,
+    NgayXem DATETIME,
+    TrangThaiXem NVARCHAR(50),
+    PRIMARY KEY (IdBacSi, IdThongBao),
+    FOREIGN KEY (IdBacSi) REFERENCES BacSi(IdBacSi),
+    FOREIGN KEY (IdThongBao) REFERENCES ThongBao(IdThongBao)
+);
+
+CREATE TABLE ThongBao_BenhNhan (
+    IdBenhNhan INT,
+    IdThongBao INT,
+    NgayXem DATETIME,
+    TrangThaiXem NVARCHAR(50),
+    PRIMARY KEY (IdBenhNhan, IdThongBao),
+    FOREIGN KEY (IdBenhNhan) REFERENCES BenhNhan(IdBenhNhan),
+    FOREIGN KEY (IdThongBao) REFERENCES ThongBao(IdThongBao)
+);
+
+------------------------------------------------------THEO DÕI ĐÁNH GIÁ-------------------------------------
+CREATE TABLE TheoDoi (
+    IdBacSi INT,
+    IdBenhNhan INT,
+    NgayBatDauTheoDoi DATE,
+    PRIMARY KEY (IdBacSi, IdBenhNhan),
+    FOREIGN KEY (IdBacSi) REFERENCES BacSi(IdBacSi),
+    FOREIGN KEY (IdBenhNhan) REFERENCES BenhNhan(IdBenhNhan)
+);
+
+CREATE TABLE DanhGia (
+    IdDanhGia INT IDENTITY PRIMARY KEY,
+    DiemDanhGia INT,
+    NoiDung NVARCHAR(MAX),
+    NgayDanhGia DATE,
+    IdBacSi INT,
+    IdBenhNhan INT,
+    FOREIGN KEY (IdBacSi) REFERENCES BacSi(IdBacSi),
+    FOREIGN KEY (IdBenhNhan) REFERENCES BenhNhan(IdBenhNhan)
+);
+------------------------------------------------------LỊCH SỬ – THỐNG KÊ – BÁO CÁO-------------------------------------
+CREATE TABLE LichSuTimKiem (
+    IdTimKiem INT IDENTITY PRIMARY KEY,
+    TuKhoaTK NVARCHAR(255),
+    ThoiGianTK DATETIME,
+    ViTriTimKiem NVARCHAR(255),
+    IdBenhNhan INT,
+    FOREIGN KEY (IdBenhNhan) REFERENCES BenhNhan(IdBenhNhan)
+);
+
+CREATE TABLE ThongKe (
+    IdThongKe INT IDENTITY PRIMARY KEY,
+    NgayThongKe DATE,
+    ChuKyThongKe NVARCHAR(50),
+    DoiTuongThongKe NVARCHAR(100),
+    SoLuotTimKiem INT,
+    LuotXemHoSo INT,
+    LuotYeuThich INT,
+    TopTuKhoaTimKiem NVARCHAR(255),
+    PhamTramTimKiemLoc FLOAT
+);
+
+CREATE TABLE BaoCao (
+    IdBaoCao INT IDENTITY PRIMARY KEY,
+    NoiDung NVARCHAR(MAX),
+    LoaiBaoCao NVARCHAR(100),
+    NgayTaoBaoCao DATE
+);
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # TimKiemThongTinBacSi
+Nhóm 12
+Phạm Minh Huy
+Dương Công Tiến
+Nguyễn Phước Quý Bửu
+Tạ Quang Nhựt
